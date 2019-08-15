0649d81b88314ffff442a5c1c0831d49bfed0191
Abdulhakeem Adetunji Mustapha <abdulhakeemmustapha@gmail.com>
2019-08-15 10:28:46 +0000

    Fix to any number greater than 0.

--- romanNumbers.py
+++ romanNumbers.py
@@ -1,7 +1,27 @@
+numerals = {
+    1000: 'M',
+    500: 'D',
+    100: 'C',
+    50: 'L',
+    10: 'X',
+    5: 'V',
+    1: 'I'
+}
+
+
+def indian_to_roman(number):
+    roman = ''
+    for numeral in numerals.keys():
+        repeat = int(number / numeral)
+        if repeat > 0:
+            roman += repeat * numerals[numeral]
+        number %= numeral
+    return roman
+
+
 def roman():
-    romanNumerals = ["I","II","III","IV","V","VI","VII","IIX","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX","XXI","XXII","XXIII","XXIV","XXV","XXVI","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV","XXXV","XXXVI","XXXVII","XXXVIII","XXXIX","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L"]
     num = int(input("Enter an Intiger number between 0-50: "))
-    if (num > 0 and num <= 50):
-        print(romanNumerals[num-1])
+    if (num > 0):
+        print(indian_to_roman(number))
     else:
-        print("Error")
+        print("Error, number has to be 1 or greater.")
